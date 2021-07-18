bf9244db3299bd44c22e097f06ca5098a3e667e6
darioMiskovic <dario12898@gmail.com>
2021-07-18 14:25:35 +0200

    first commit

---
+++ src/app/recipes/models/recipeInfo.model.ts
@@ -0,0 +1,10 @@
+export interface RecipeInfoModel{
+  publisher: string;
+  ingredients: string[];
+  source_url: string;
+  recipe_id: string;
+  image_url: string;
+  social_rank: number,
+  publisher_url: string;
+  title: string;
+}
