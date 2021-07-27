f8e53efc2cee217c18260133f16ec759952571f6
darioMiskovic <dario12898@gmail.com>
2021-07-27 12:34:41 +0200

    modified design

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,35 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from "@angular/forms";
-import {LoginFormComponent} from "./login-form/login-form.component";
-import {RegisterFormComponent} from "./register-form/register-form.component";
-import {AuthComponent} from "./auth.component";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AppRoutingModule} from "../app-routing.module";
-import {AuthInterceptorService} from "./auth-interceptor.service";
-import {SharedModule} from "../shared/shared.module";
-
-
+import { FormsModule } from '@angular/forms';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from '../app-routing.module';
+import { AuthInterceptorService } from './auth-interceptor.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    LoginFormComponent,
-    RegisterFormComponent,
-    AuthComponent
-  ],
+  declarations: [LoginFormComponent, RegisterFormComponent],
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
-  providers:[
+  providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,127 +1,178 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {RecipesService} from "../recipes/recipes.service";
-import {RecipeModel} from "../recipes/models/recipe.model";
-import {BehaviorSubject} from "rxjs";
-import {map, switchAll} from "rxjs/operators";
-import {RecipeInfoModel} from "../recipes/models/recipeInfo.model";
-import {IngredientInfoModel} from "../recipes/models/ingredientInfo.model";
+import { HttpClient } from '@angular/common/http';
+import { RecipesService } from '../recipes/recipes.service';
+import { RecipeModel } from '../recipes/models/recipe.model';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RecipeInfoModel } from '../recipes/models/recipeInfo.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStorageService {
   currentUser = new BehaviorSubject(null);
   currentUserID!: number;
 
-  constructor(private http: HttpClient, private recipesService: RecipesService) { }
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipesService
+  ) {}
 
   //Recipe service
-  fetchBookmarks(){
+  fetchBookmarks() {
     // @ts-ignore
-    this.currentUser.subscribe(user => this.currentUserID = user.id)
-    this.http.get("http://127.0.0.1:8000/api/bookmark").subscribe((res: any) => {
-      if (res?.split('<>').length > 0){
-        const bookmarks = res.split('<>');
-        bookmarks.pop();
-        this.recipesService.myBookmarks = bookmarks.map((bkm: string) => JSON.parse(bkm))
-        this.recipesService.myBookmarksUpdated.next(this.recipesService.myBookmarks);
-      }else{
-        this.recipesService.myBookmarks = [];
-        this.recipesService.myBookmarksUpdated.next(this.recipesService.myBookmarks);
-      }
-    })
+    this.currentUser.subscribe((user) => (this.currentUserID = user.id));
+    this.http
+      .get('http://127.0.0.1:8000/api/bookmark')
+      .subscribe((res: any) => {
+        if (res?.split('<>').length > 0) {
+          const bookmarks = res.split('<>');
+          bookmarks.pop();
+          this.recipesService.myBookmarks = bookmarks.map((bkm: string) =>
+            JSON.parse(bkm)
+          );
+          this.recipesService.myBookmarksUpdated.next(
+            this.recipesService.myBookmarks
+          );
+        } else {
+          this.recipesService.myBookmarks = [];
+          this.recipesService.myBookmarksUpdated.next(
+            this.recipesService.myBookmarks
+          );
+        }
+      });
   }
 
-  addBookmark(bookmark: RecipeModel){
+  addBookmark(bookmark: RecipeModel) {
     // @ts-ignore
-    this.currentUser.subscribe(user => this.currentUserID = user.id)
-    this.http.post('http://127.0.0.1:8000/api/bookmark', {bookmark: JSON.stringify(bookmark)+'<>'})
-      .subscribe((res:any) => {
-        console.log(res);
-        this.recipesService.myBookmarks.push(bookmark);
-        this.recipesService.myBookmarksUpdated.next(this.recipesService.myBookmarks);
-      }, error => {
-        console.log(error)
+    this.currentUser.subscribe((user) => (this.currentUserID = user.id));
+    this.http
+      .post('http://127.0.0.1:8000/api/bookmark', {
+        bookmark: JSON.stringify(bookmark) + '<>',
       })
+      .subscribe(
+        (res: any) => {
+          console.log(res);
+          this.recipesService.myBookmarks.push(bookmark);
+          this.recipesService.myBookmarksUpdated.next(
+            this.recipesService.myBookmarks
+          );
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
-  removeBookmark(removeBookmarkID: string){
-    const removeIndex = this.recipesService.myBookmarks.findIndex(bookmark => bookmark.id === removeBookmarkID);
+  removeBookmark(removeBookmarkID: string) {
+    const removeIndex = this.recipesService.myBookmarks.findIndex(
+      (bookmark) => bookmark.id === removeBookmarkID
+    );
     this.recipesService.myBookmarks.splice(removeIndex, 1);
-    const bookmarksArrStringify = this.recipesService.myBookmarks.map(bm => JSON.stringify(bm)+'<>').join('');
+    const bookmarksArrStringify = this.recipesService.myBookmarks
+      .map((bm) => JSON.stringify(bm) + '<>')
+      .join('');
 
-    this.http.post('http://127.0.0.1:8000/api/bookmark/delete', {
-      bookmarks: bookmarksArrStringify
-    }).subscribe((res:any) => {
-      console.log(res);
-      this.recipesService.myBookmarksUpdated.next(this.recipesService.myBookmarks);
-    }, error => {
-      console.log(error)
-    })
+    this.http
+      .post('http://127.0.0.1:8000/api/bookmark/delete', {
+        bookmarks: bookmarksArrStringify,
+      })
+      .subscribe(
+        (res: any) => {
+          console.log(res);
+          this.recipesService.myBookmarksUpdated.next(
+            this.recipesService.myBookmarks
+          );
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 
   //Recipe component
-  fetchRecipeDetail(id: string){
-   return  this.http.get<RecipeInfoModel>(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`).pipe(
-      map((recipe: any) => {
-        return recipe.data.recipe;
-      })
-    )
+  fetchRecipeDetail(id: string) {
+    return this.http
+      .get<RecipeInfoModel>(
+        `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      )
+      .pipe(
+        map((recipe: any) => {
+          return recipe.data.recipe;
+        })
+      );
   }
 
   //Header component
-  fetchMyRecipes(){
+  fetchMyRecipes() {
     this.recipesService.loadSpinner.next(true);
-    this.http.get('http://127.0.0.1:8000/api/my-recipe').subscribe((res:any) => {
-
-      let myRecipes = res.split('<>');
-      myRecipes.pop();
-      myRecipes = myRecipes.map((recipe: string) => JSON.parse(recipe));
-      this.recipesService.loadSpinner.next(false);
-      this.recipesService.myRecipeSearch.next(myRecipes);
-      //Store my recipes in recipes service
-      this.recipesService.myRecipes = myRecipes;
-
-    }, error => {
-      this.recipesService.loadSpinner.next(false);
-      this.recipesService.myRecipes = [];
-      this.recipesService.myRecipeSearch.next([]);
-    });
+    this.http.get('http://127.0.0.1:8000/api/my-recipe').subscribe(
+      (res: any) => {
+        let myRecipes = res.split('<>');
+        myRecipes.pop();
+        myRecipes = myRecipes.map((recipe: string) => JSON.parse(recipe));
+        this.recipesService.loadSpinner.next(false);
+        this.recipesService.myRecipeSearch.next(myRecipes);
+        //Store my recipes in recipes service
+        this.recipesService.myRecipes = myRecipes;
+      },
+      (error) => {
+        this.recipesService.loadSpinner.next(false);
+        this.recipesService.myRecipes = [];
+        this.recipesService.myRecipeSearch.next([]);
+      }
+    );
   }
 
   //Add recipe component
-  myRecipesUpdate(update: boolean, myRecipe: RecipeInfoModel, recipeID = 0, deleteMyRecipe = false){
-
-    if(update){
-      if(deleteMyRecipe){
-        const removeIndex = this.recipesService.myRecipes.findIndex(recipe => +recipe.id === recipeID);
+  myRecipesUpdate(
+    update: boolean,
+    myRecipe: RecipeInfoModel,
+    recipeID = 0,
+    deleteMyRecipe = false
+  ) {
+    if (update) {
+      if (deleteMyRecipe) {
+        const removeIndex = this.recipesService.myRecipes.findIndex(
+          (recipe) => +recipe.id === recipeID
+        );
         this.recipesService.myRecipes.splice(removeIndex, 1);
-      }else{
+      } else {
         this.recipesService.myRecipes[recipeID] = myRecipe;
       }
 
-      const updatedMyRecipesArr = this.recipesService.myRecipes.map(recipe => JSON.stringify(recipe)+'<>').join('');
-      return this.http.post('http://127.0.0.1:8000/api/my-recipe/update', {my_recipe: updatedMyRecipesArr})
-        .pipe(map( (response: any) => {
-          return {
-            type: response.split(' ')[2],
-            resMessage: response,
-            deleteRecipe: deleteMyRecipe,
-            recipeID: myRecipe.id
-          }
-        }))
+      const updatedMyRecipesArr = this.recipesService.myRecipes
+        .map((recipe) => JSON.stringify(recipe) + '<>')
+        .join('');
+      return this.http
+        .post('http://127.0.0.1:8000/api/my-recipe/update', {
+          my_recipe: updatedMyRecipesArr,
+        })
+        .pipe(
+          map((response: any) => {
+            return {
+              type: response.split(' ')[2],
+              resMessage: response,
+              deleteRecipe: deleteMyRecipe,
+              recipeID: myRecipe.id,
+            };
+          })
+        );
     }
 
     this.recipesService.myRecipes.push(myRecipe);
     this.recipesService.myRecipeSearch.next(this.recipesService.myRecipes);
-     return this.http.post('http://127.0.0.1:8000/api/my-recipe', {my_recipe: (JSON.stringify(myRecipe)+'<>')})
-       .pipe(map( (response: any) => {
-         return {
-           type: response.split(' ')[3],
-           resMessage: response
-         }
-       }))
+    return this.http
+      .post('http://127.0.0.1:8000/api/my-recipe', {
+        my_recipe: JSON.stringify(myRecipe) + '<>',
+      })
+      .pipe(
+        map((response: any) => {
+          return {
+            type: response.split(' ')[3],
+            resMessage: response,
+          };
+        })
+      );
   }
-
 }
